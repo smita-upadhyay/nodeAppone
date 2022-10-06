0c69abd1b9c2d41d925a42c5acde40be25a03155
smita-upadhyay <78595758+smita-upadhyay@users.noreply.github.com>
2022-10-06 18:45:36 +0100

    Update and rename script to app.sh

---
+++ app.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+sleep 30
+
+# Install node js
+sudo apt update
+sudo apt install -y nodejs
+sudo apt install -y npm
+
+# Install pm2
+sudo npm install -g pm2
+
+
+#git clone
+git clone https://github.com/smita-upadhyay/nodeAppone.git
+
+#react  config
+cd nodeAppone
+npm install
+npm run build
+
+#configurationpm2 and serve
+sudo npm install serve --global
+sudo pm2 serve build 3000 --spa
+sudo pm2 startup
+sudo pm2 save
+
+#nodejs config
+cd NodeJS
+npm install
+sudo pm2 start server.js
+sudo pm2 startup
+sudo pm2 save
